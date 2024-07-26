b80c0a3cc6febe859f1fe48e64e683c43eb308a1
unknown <sumanuphuket@gmail.com>
2024-07-26 08:13:30 +0545

    project setup

---
+++ classes/Employee.php
@@ -0,0 +1,12 @@
+<?php
+
+class User{
+    private $NAME;
+    private $ADDRESS;
+    private $EMAIL;
+    private $MOBILE;
+    private $USERNAME;
+    private $PASSWORD;
+    private $TYPE;
+    private $STATUS
+}
---
+++ classes/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+class User{
+    private $HOST -'localhost';
+    private $USER;
+    private $PASSWORD;
+    private $DATABASE;
+   
+}
+
+function __ construct __()
+
+{
+    $this $HOST='local'
+}
